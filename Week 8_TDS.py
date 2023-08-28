a3863d805f776b5bf841887880925c97f2a09dd6
Rx610 <136077618+Rx610@users.noreply.github.com>
2023-08-28 15:21:32 +0530

    Update Week 8_TDS.py

--- Week 8_TDS.py
+++ Week 8_TDS.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-def largest_number(a, b, c):
-    return max(a, b, c)
-
 st.title("Find the Largest Number")
     
 st.write("Enter three numbers:")
@@ -11,5 +8,5 @@ b = st.number_input("Enter the second number:")
 c = st.number_input("Enter the third number:")
     
 if st.button("Find Largest"):
-largest = largest_number(a, b, c)
+largest = max(a, b, c)
 st.write(f"The largest number is: {largest}")
